3b97d909289bd2d768baea48f4efbb50cfbe0875
Stella-055 <stellaongayo243@gmail.com>
2025-05-27 20:40:28 +0300

    ensured responsiveness formatted code

--- src/main.js
+++ src/main.js
@@ -6,33 +6,33 @@ searchButton.addEventListener("click", async function (e) {
   e.preventDefault();
   let searchTerm = inputValue.value.trim();
   if (searchTerm) {
-    resultDiv.innerHTML = `<p> <b>${searchTerm}</b>  is probably from....</p>`;  
-} else {
+    resultDiv.innerHTML = `<p> <b>${searchTerm}</b>  is probably from....</p>`;
+  } else {
     resultDiv.innerHTML = "<p>Please enter a name.</p>";
-}
-   try{
-     let response = await fetch(`https://api.nationalize.io/?name=${searchTerm}`);
-      if (!response.ok) {
-          throw new Error("Network response was not ok");
-      }
-      let {country} = await response.json();
-     
-      if (country.length === 0) {
-          resultDiv.innerHTML = `<p>Could not trace <b>${searchTerm}</b>.</p>`;
-          return;
-      }
-      else{
-        resultDiv.innerHTML ="";
-      }
-     country.forEach(element => {
-        resultDiv.innerHTML += `<p>Country: <b>${element.country_id}</b> - Probability: <b>${(element.probability * 100).toFixed(2)}%</b></p>`;
-     });
+  }
+  try {
+    let response = await fetch(
+      `https://api.nationalize.io/?name=${searchTerm}`
+    );
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    let { country } = await response.json();
 
-
-    
-   }
-   catch (error) {
-      console.log("Error fetching data:", error);
-   }
-}
-)
+    if (country.length === 0) {
+      resultDiv.innerHTML = `<p>Could not trace <b>${searchTerm}</b>.</p>`;
+      return;
+    } else {
+      resultDiv.innerHTML = "";
+    }
+    country.forEach((element) => {
+      resultDiv.innerHTML += `<p>From: <b>${
+        element.country_id
+      }</b> - Probability: <b>${(element.probability * 100).toFixed(
+        2
+      )}%</b></p>`;
+    });
+  } catch (error) {
+    console.log("Error fetching data:", error);
+  }
+});
